929af901ca4b2aafe4d8b93dd8c6e62a2a5abf5b
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2009-11-05 22:01:46 +0000

    First set of classes for unsuck.

---
+++ src/unsuck/collections/SetUtils.java
@@ -0,0 +1,50 @@
+/*
+ * $Id$
+ */
+
+package unsuck.collections;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * Some basic utilities for manipulating sets
+ * 
+ * @author Jeff Schnitzer
+ */
+public class SetUtils
+{
+	/**
+	 * Constructs a union set of the collections, without the irritating unchecked warning
+	 * of the varargs method.  Treats null parameters as empty collections.
+	 */
+	public static <T> Set<T> union(Collection<T> c1, Collection<T> c2)
+	{
+		Set<T> union = new HashSet<T>();
+		
+		if (c1 != null)
+			union.addAll(c1);
+		
+		if (c2 != null)
+			union.addAll(c2);
+		
+		return union;
+	}
+	
+	/**
+	 * Puts all the elements in a set
+	 */
+	public static <T> Set<T> asSet(T[] array)
+	{
+		if (array == null)
+			return null;
+		
+		Set<T> result = new HashSet<T>(array.length * 2);
+		for (T item: array)
+			result.add(item);
+		
+		return result;
+	}
+}
---
+++ src/unsuck/web/URLUtils.java
@@ -0,0 +1,54 @@
+/*
+ * $Id$
+ */
+
+package unsuck.web;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+/**
+ * Some basic utilities for manipulating urls.
+ * 
+ * @author Jeff Schnitzer
+ */
+public class URLUtils
+{
+	/**
+	 * Stupid java doesn't come with a URL builder.
+	 */
+	public static String buildURL(String base, Map<String, Object> params)
+	{
+		StringBuilder bld = new StringBuilder();
+		bld.append(base);
+		
+		if (params != null && !params.isEmpty())
+		{
+			boolean useAmpersand = false;
+			for (Map.Entry<String, Object> entry: params.entrySet())
+			{
+				bld.append(useAmpersand ? "&" : "?");
+				useAmpersand = true;
+				
+				bld.append(urlEncode(entry.getKey()));
+				bld.append("=");
+				bld.append(urlEncode(entry.getValue()));
+			}
+		}
+		
+		return bld.toString();
+	}
+	
+	/**
+	 * An interface to URLEncoder.encode() that isn't inane
+	 */
+	public static String urlEncode(Object value)
+	{
+		try
+		{
+			return URLEncoder.encode(value.toString(), "utf-8");
+		}
+		catch (UnsupportedEncodingException e) { throw new RuntimeException(e); }
+	}
+}
