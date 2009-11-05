01a4233cf101423d9bcf279ddc08cd7f1a55045f
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2009-11-05 22:31:14 +0000

    Implemented the DecoderRing.

--- src/unsuck/security/DecoderRing.java
+++ src/unsuck/security/DecoderRing.java
@@ -4,27 +4,35 @@
 
 package unsuck.security;
 
+import java.io.IOException;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
+import org.codehaus.jackson.map.ObjectMapper;
+
 /**
  * Secret decoder ring which allows us to sign and verify the signature
- * of remote calls.  Contains the shared secret.
+ * of remote calls.
+ * 
+ * Depends on Jackson and DigestOutputStream (thus commons-codec).
  * 
  * @author Jeff Schnitzer
  */
 public class DecoderRing
 {
-	/** The shared secret */
-	private static final String KEY = "somemadeupbullshitkey";
-	
 	/** */
 	protected SortedMap<String, Object> params = new TreeMap<String, Object>();
 	
-	/** Starts a decoder ring for a given method */
-	public DecoderRing(String method)
+	/** Starts a decoder ring with a secret */
+	public DecoderRing(String secret)
 	{
-		this.params.put("_method", method);
+		this.params.put("_secret", secret);
+	}
+	
+	/** */
+	public void addMethod(String name)
+	{
+		this.params.put("_method", name);
 	}
 	
 	/** Don't add a timestamp or signature */
@@ -36,19 +44,36 @@ public class DecoderRing
 	/** Create a signature for the given timestamp */
 	public String sign(long timestamp)
 	{
-		if (this.params.containsKey("_timestamp"))
-			throw new IllegalStateException("You can only sign once");
-
-		this.params.put("_timestamp", timestamp);
-		this.params.put("_secret", KEY);
-		
-		// We turn this whole thing into a JSON representation and sign that
-		
-		return null;
+		return this.digest(timestamp);
 	}
 	
 	/** Verify a signature, throwing IllegalArgumentException on failure */
 	public void verify(long timestamp, String signature) throws IllegalArgumentException
 	{
+		if (!signature.equals(this.digest(timestamp)))
+			throw new IllegalArgumentException("Bad signature");
+	}
+	
+	/**
+	 * @return the digest of this thing after adding timestamp
+	 */
+	private String digest(long timestamp)
+	{
+		if (this.params.containsKey("_timestamp"))
+			throw new IllegalStateException("You can only sign/verify once");
+
+		this.params.put("_timestamp", timestamp);
+		
+		// We turn this whole thing into a JSON representation and sign that
+		ObjectMapper mapper = new ObjectMapper();
+		DigestOutputStream digester = new DigestOutputStream("SHA1");
+		
+		try
+		{
+			mapper.writeValue(digester, this.params);
+		}
+		catch (IOException e) { throw new RuntimeException(e); }
+		
+		return digester.getDigestHex();
 	}
 }
