8a99dcfcb971ac9fced13d49a5923d36456a7650
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2009-11-16 06:24:41 +0000

    Added a BasicAuthFilter.

---
+++ trunk/src/unsuck/security/BasicAuthFilter.java
@@ -0,0 +1,101 @@
+/*
+ * $Id$
+ */
+
+package unsuck.security;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLConnection;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import unsuck.web.AbstractFilter;
+
+
+/**
+ * <p>A filter which requires very simple basic auth.  Just derive a subclass
+ * that implements the getRealm() and authenticate() methods.</p>  
+ * 
+ * <p>Does not interact with the J2EE security constraints
+ * in any way, so it can be used to enable basic auth in Google App Engine.</p>
+ * 
+ * <p>Requires commons-codec.</p>
+ * 
+ * @author Jeff Schnitzer
+ */
+abstract public class BasicAuthFilter extends AbstractFilter
+{
+	/** */
+	private final static Logger log = LoggerFactory.getLogger(BasicAuthFilter.class);
+	
+	/** */
+	String authenticateHeader;
+	
+	/** @return the ream which is advertised to clients */
+	abstract public String getRealm();
+	
+	/** @return true if the username/pw combo are valid */
+	abstract public boolean authenticate(String username, String password);
+	
+	/** Useful tool:  Add http basic auth credentials to a connection */
+	public static void addBasicAuth(URLConnection conn, String username, String password)
+	{
+		try
+		{
+			StringBuilder buf = new StringBuilder(username).append(':').append(password);
+			
+			// There is no standard for charset, might as well use utf-8
+			byte[] bytes = buf.toString().getBytes("utf-8");
+
+			String header = "Basic " + Base64.encodeBase64String(bytes);
+			conn.setRequestProperty("Authorization", header);
+		}
+		catch (UnsupportedEncodingException ex) { throw new RuntimeException(ex); }
+	}
+
+	/** */
+	@Override
+	public void init(FilterConfig cfg)
+	{
+		super.init(cfg);
+		
+		this.authenticateHeader = "Basic realm=\"" + this.getRealm() + "\"";
+	}
+
+	/** */
+	@Override
+	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException
+	{
+		String authorization = request.getHeader("Authorization");
+		if (authorization != null && authorization.startsWith("Basic "))
+		{
+			String base64AuthInfo = authorization.substring("Basic ".length());
+			// There is no charset standard for basic auth, utf-8 is as good as any
+			String authInfo = new String(Base64.decodeBase64(base64AuthInfo), "utf-8");
+			String[] authParts = authInfo.split(":");
+			
+			if (this.authenticate(authParts[0], authParts[1]))
+			{
+				chain.doFilter(request, response);
+				return;
+			}
+			else
+			{
+				log.warn("Failed auth attempt for: " + authParts[0]);
+			}
+		}
+
+		// return auth required
+		response.addHeader("WWW-Authenticate", this.authenticateHeader);
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+	}
+}
