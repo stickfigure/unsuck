0cfb7938130bf5bc5211c66ebd6bc3e2874de2fb
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2009-12-08 01:06:19 +0000

    Added PrettyURLFilter

---
+++ src/unsuck/web/PrettyURLFilter.java
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ */
+
+package unsuck.web;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Any urls that get mapped to this filter will be forwarded to their .jsp equivalent.
+ * Ie, /map -> /map.jsp.
+ * 
+ * It is a filter rather than a servlet so that if a dispatcher cannot be found, the
+ * request is passed through so whatever normal 404 mechanism can take place.
+ * 
+ * @author Jeff Schnitzer
+ */
+public class PrettyURLFilter extends AbstractFilter
+{
+	/** */
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(PrettyURLFilter.class);
+
+	@Override
+	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException
+	{
+		String path = request.getServletPath();
+		RequestDispatcher disp = request.getRequestDispatcher(path + ".jsp");
+		if (disp == null)
+			chain.doFilter(request, response);
+		else
+			disp.forward(request, response);
+	}
+}
