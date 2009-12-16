1a344fd53054afe5bf97885e4f31e517eea853d7
latchkey <latchkey@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2009-12-16 08:34:07 +0000

    add another utility fromJSON method that accepts jackson TypeReference objects. bit of yucky duplication of code, not sure how to clean it up.

--- trunk/src/unsuck/json/JSONUtils.java
+++ trunk/src/unsuck/json/JSONUtils.java
@@ -7,11 +7,12 @@ package unsuck.json;
 import java.io.StringWriter;
 
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 
 
 /**
  * Some basic utilities for processing JSON.  This class depends on Jackson.
- * 
+ *
  * @author Jeff Schnitzer
  */
 public class JSONUtils
@@ -25,11 +26,11 @@ public class JSONUtils
 		try
 		{
 			StringWriter writer = new StringWriter();
-			
+
 			mapper.writeValue(writer, value);
-			
+
 			return writer.toString();
-			
+
 			// This requires Jackson 1.3 which breaks GAE.
 			// TODO, replace with this code when bug is fixed:
 			// http://jira.codehaus.org/browse/JACKSON-188
@@ -38,7 +39,7 @@ public class JSONUtils
 		catch (RuntimeException ex) { throw ex; }
 		catch (Exception ex) { throw new RuntimeException(ex); }
 	}
-	
+
 	/**
 	 * Converts the JSON string to an Object (either Map or List)
 	 */
@@ -46,7 +47,25 @@ public class JSONUtils
 	{
 		return fromJSON(value, Object.class);
 	}
-	
+
+	/**
+	 * Converts the JSON string to a typed object via a TypeReference
+	 * The main complication is handling of Generic types: if they are used, one
+	 * has to use TypeReference object, to work around Java Type Erasure.
+	 *
+	 * ex: return JSONUtils.fromJSON(this.answersJson, new TypeReference<List<StanzaAnswer>>(){});
+	 */
+	public static <T> T fromJSON(String value, TypeReference<T> type)
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		try
+		{
+			return mapper.readValue(value, type);
+		}
+		catch (RuntimeException ex) { throw ex; }
+		catch (Exception ex) { throw new RuntimeException(ex); }
+	}
+
 	/**
 	 * Converts the JSON string to a typed object (or Map/List if Object.class is passed in)
 	 */
