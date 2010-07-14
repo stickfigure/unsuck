627119a9a4a2b10458ddebb4899b611319ede4dd
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2010-07-14 22:53:31 +0000

    Replaced the FastLooseList with a simpler and more useful FastLooseMap.

---
+++ trunk/src/unsuck/collections/FastLooseMap.java
@@ -0,0 +1,176 @@
+/*
+ */
+
+package unsuck.collections;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>Much like a LinkedHashMap, but has some unusual characteristics and a simplified
+ * interface to make implementation easier.</p>
+ * 
+ * <p>First and foremost, this class has *very* efficient iteration over values.
+ * It uses a linked list that plays fast and lose with thread safety; writes are
+ * synchronized but iteration is not.  There is no check for comodifcation,
+ * just a sort of "eventual consistency" on iteration.</p>
+ * 
+ * <p>This class is not guaranteed to work in all JVMs or with all compilers due
+ * to memory caching and instruction reordering optimizations.  However, it
+ * should work "well enough" in the Sun JVM on Intel.</p>
+ * 
+ * <p>There is *no* guarantee on iteration order.</p>
+ */
+public class FastLooseMap<K, V> implements Iterable<V>
+{
+	/** */
+	@SuppressWarnings("unused")
+	private final static Logger log = LoggerFactory.getLogger(FastLooseMap.class);
+
+	/**
+	 * The actual list nodes.
+	 */
+	static class Node<V>
+	{
+		V payload;
+		Node<V> next;
+		Node<V> previous;
+	}
+	
+	/**
+	 * Implements the fast unsynchronized iterator across values
+	 */
+	class IteratorImpl implements Iterator<V>
+	{
+		Node<V> current;
+		
+		IteratorImpl(Node<V> node)
+		{
+			current = node;
+		}
+
+		@Override
+		public boolean hasNext()
+		{
+			return current.next != null;
+		}
+
+		@Override
+		public V next()
+		{
+			assert current.next != null;
+			
+			current = current.next;
+			return current.payload;
+		}
+
+		@Override
+		public void remove()
+		{
+			// Not hard to implement, but we would need to add the key to the Node
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	/**
+	 * There is one bogus head node to make the algorithm easy.  The bogus node
+	 * will start with previous and next pointing to itself, so the last node
+	 * will always point to head (not null).
+	 */
+	Node<V> head;
+	
+	/**
+	 * A map of key to node.  Doesn't need to be synchronized.
+	 */
+	Map<K, Node<V>> nodeMap = new HashMap<K, Node<V>>();
+	
+	/**
+	 */
+	public FastLooseMap()
+	{
+		this.clear();
+	}
+	
+	/**
+	 * Gets the value at the associated key, or null if there is none.
+	 * Note that this method is synchronized and probably shouldn't be
+	 * used in performance-sensitive places.  It must be synchronized
+	 * because the nodemap isn't. 
+	 */
+	public synchronized V get(K key)
+	{
+		Node<V> node = this.nodeMap.get(key);
+		if (node == null)
+			return null;
+		else
+			return node.payload;
+	}
+	
+	/**
+	 * Adds or replaces the value.  Note that iteration order is never guaranteed.
+	 */
+	public synchronized void put(K key, V value)
+	{
+		Node<V> node = this.nodeMap.get(key);
+		if (node == null)
+		{
+			node = new Node<V>();
+			node.next = this.head.next;
+			node.previous = this.head;
+			this.head.next = node;
+			
+			this.nodeMap.put(key, node);
+		}
+		
+		node.payload = value;
+	}
+	
+	/**
+	 * Remove the entry from the list
+	 */
+	public synchronized V remove(K key)
+	{
+		Node<V> node = this.nodeMap.get(key);
+		if (node == null)
+			return null;
+		else
+		{
+			node.previous.next = node.next;
+			node.next.previous = node.previous;
+			
+			this.nodeMap.remove(key);
+			
+			return node.payload;
+		}
+	}
+	
+	/**
+	 * Create a nice fast iterator on this list
+	 */
+	public Iterator<V> iterator()
+	{
+		return new IteratorImpl(this.head);
+	}
+	
+	/**
+	 */
+	public synchronized void clear()
+	{
+		this.head = new Node<V>();
+
+		this.nodeMap.clear();
+	}
+
+	/**
+	 * @return the current number of elements in the list
+	 */
+	public int size()
+	{
+		return this.nodeMap.size();
+	}
+
+}
