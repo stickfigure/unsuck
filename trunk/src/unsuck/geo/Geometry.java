d0a69d3932da747f70714def451377f97d0ad7f5
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2011-03-11 02:17:50 +0000

    Added Geometry method for calculating distance using degrees

--- trunk/src/unsuck/geo/Geometry.java
+++ trunk/src/unsuck/geo/Geometry.java
@@ -29,15 +29,39 @@ public class Geometry
 	 *
 	 * @author http://www.mercury.demon.co.uk/dist/formula.html
 	 */
-	public static double calculateDistance(double latA, double lonA, double latB, double lonB)
+	public static double calculateDistanceRadians(double latA, double lonA, double latB, double lonB)
 	{
 		double p1 = Math.cos(latA) * Math.cos(lonA) * Math.cos(latB) * Math.cos(lonB);
-		
 		double p2 = Math.cos(latA) * Math.sin(lonA) * Math.cos(latB) * Math.sin(lonB);
-		
 		double p3 = Math.sin(latA) * Math.sin(latB);
 		
 		return Math.acos(p1 + p2 + p3) * EARTH_RADIUS;
+	}
+
+	/**
+	 * Calculates the distance between two latitude, longitude values
+	 * using a great circle computation.  Note that the algoritm assumes
+	 * the earth to be a perfect sphere, whereas in fact the equatorial
+	 * radius is about 30Km greater than the polar.
+	 *
+	 * All parameters must be in degrees.
+	 * 
+	 * Note that this code is totally different than the other code.  Hell if I know why.
+	 *
+	 * @return the distance in Kilometers.
+	 *
+	 * @author http://stackoverflow.com/questions/120283/working-with-latitude-longitude-values-in-java
+	 */
+	public static double calculateDistanceDegrees(double latA, double lonA, double latB, double lonB)
+	{
+		double dLat = Math.toRadians(latB - latA);
+		double dLng = Math.toRadians(lonB - lonA);
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(Math.toRadians(latA))
+				* Math.cos(Math.toRadians(latB)) * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double dist = EARTH_RADIUS * c;
+
+		return dist;
 	}
 }
 
