bbb0dc5408a520d88f36bf5554760f3af3eed808
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2011-08-10 00:13:46 +0000

    Oops, Nullable is not a binding annotation

--- src/unsuck/guice/Nullable.java
+++ src/unsuck/guice/Nullable.java
@@ -5,12 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import com.google.inject.BindingAnnotation;
-
 /**
  * Guice will recognize this and allow null injection.  Any annotation named "Nullable" is magic.
  */
-@BindingAnnotation
 @Target({ ElementType.FIELD, ElementType.PARAMETER })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Nullable {}
