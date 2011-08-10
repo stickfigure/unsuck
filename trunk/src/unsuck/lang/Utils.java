77792ad2d777ab963bd3af57588d03d111d243fa
latchkey <latchkey@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2011-08-10 06:31:01 +0000

    delete stringutils. move method into Utils. add method for breaking down words into a set.

--- trunk/src/unsuck/lang/Utils.java
+++ trunk/src/unsuck/lang/Utils.java
@@ -1,5 +1,8 @@
 package unsuck.lang;
 
+import java.util.Arrays;
+import java.util.Set;
+
 
 /**
  * Some basic stupid java tools.
@@ -11,6 +14,23 @@ public class Utils
 	 */
 	public static boolean safeEquals(Object o1, Object o2)
 	{
-		return o1 == o2 || (o1 != null && o1.equals(o2));
+		return (o1 == o2) || ((o1 != null) && o1.equals(o2));
+	}
+
+	/**
+	 * Throws exception if value is null or empty or whitespace
+	 */
+	public static void requireNotBlank(String value)
+	{
+		if ((value == null) || (value.trim().length() == 0))
+			throw new IllegalArgumentException("Value cannot be null");
+	}
+
+	/**
+	 * Splits name into words. Normalizes to lower case.
+	 */
+	public static void breakdown(String name, Set<String> words) {
+		String[] tokens = name.toLowerCase().split(" ");
+		words.addAll(Arrays.asList(tokens));
 	}
 }
