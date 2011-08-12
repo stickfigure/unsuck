86ce739be9a803c40c73635563fca55d6dfdd943
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2011-08-12 23:51:00 +0000

    Reasonably happy with the LogginInterceptor now

--- src/unsuck/guice/LoggingInterceptor.java
+++ src/unsuck/guice/LoggingInterceptor.java
@@ -1,10 +1,12 @@
 package unsuck.guice;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import java.lang.reflect.Field;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * <p>Guice AOP interceptor which looks for {@code @LogCall} annotation on methods to log method calls.</p>
@@ -17,7 +19,7 @@ import org.aopalliance.intercept.MethodInvocation;
 public class LoggingInterceptor implements MethodInterceptor
 {
 	/** Default fallback logger */
-	private static Logger log = Logger.getLogger(LoggingInterceptor.class.getName());
+	private static Logger log = LoggerFactory.getLogger(LoggingInterceptor.class.getName());
 	
 	/* (non-Javadoc)
 	 * @see org.aopalliance.intercept.MethodInterceptor#invoke(org.aopalliance.intercept.MethodInvocation)
@@ -25,27 +27,45 @@ public class LoggingInterceptor implements MethodInterceptor
 	@Override
 	public Object invoke(MethodInvocation inv) throws Throwable
 	{
-//		Object obj = inv.getThis();
-//		Logger logger;
-//		
-//		Class<?> clazz = obj.getClass();
-//		try {
-//			Field loggerField = clazz.getDeclaredField("log");
-//			
-//			if (Logger.class.isAssignableFrom(loggerField.getType()))
-//				logger = (Logger)loggerField.get(obj);
-//			else
-//				logger = log;
-//		} catch (NoSuchFieldException ex) {
-//			logger = log;
-//		}
+		Object obj = inv.getThis();
+		Logger logger;
+		
+		// The actual class will be the guice enhanced one, so get super
+		Class<?> clazz = obj.getClass().getSuperclass();
+		try {
+			Field loggerField = clazz.getDeclaredField("log");
+			loggerField.setAccessible(true);
+			
+			if (Logger.class.isAssignableFrom(loggerField.getType()))
+				logger = (Logger)loggerField.get(obj);
+			else
+				logger = log;
+		} catch (NoSuchFieldException ex) {
+			logger = log;
+		}
 
-		log.logp(
-			Level.FINEST,
-			inv.getClass().getName(),
-			inv.getMethod().getName(),
-			"invocation",
-			inv.getArguments());
+		StringBuilder msg = new StringBuilder();
+		
+		msg.append(clazz.getSimpleName());
+		msg.append('.');
+		msg.append(inv.getMethod().getName());
+		msg.append('(');
+		
+		for (int i=0; i<inv.getArguments().length; i++)
+		{
+			Object arg = inv.getArguments()[i];
+			if (arg instanceof String)
+				msg.append('"').append(arg).append('"');
+			else
+				msg.append(arg);
+			
+			if (i != inv.getArguments().length-1)
+				msg.append(", ");
+		}
+
+		msg.append(')');
+				
+		logger.debug(msg.toString());
 		
 		return inv.proceed();
 	}
