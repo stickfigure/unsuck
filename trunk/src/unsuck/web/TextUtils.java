55a459c4637a4e81bc388a7a3ce3803080748fff
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2011-08-18 02:28:28 +0000

    Added escapeXML to TextUtils

--- trunk/src/unsuck/web/TextUtils.java
+++ trunk/src/unsuck/web/TextUtils.java
@@ -26,17 +26,36 @@ public class TextUtils
 			switch(c)
 			{
 				case '>': buf.append("&gt;"); break;
-					
 				case '<': buf.append("&lt;"); break;
-					
 				case '\'': buf.append("&apos;"); break;
-					
 				case '"': buf.append("&quot;"); break;
-					
 				case '&': buf.append("&amp;"); break;
-					
 				case '\n': buf.append("<br />"); break;
-					
+				default: buf.append(c);
+			}
+		}
+		
+		return buf.toString(); 
+	}
+
+	/**
+	 * Escapes all xml characters, WITHOUT converting newlines to br tags.
+	 */
+	public static String escapeXML(String orig)
+	{
+		StringBuffer buf = new StringBuffer();
+		
+		for (int i=0; i<orig.length(); i++)
+		{
+			char c = orig.charAt(i);
+			
+			switch(c)
+			{
+				case '>': buf.append("&gt;"); break;
+				case '<': buf.append("&lt;"); break;
+				case '\'': buf.append("&apos;"); break;
+				case '"': buf.append("&quot;"); break;
+				case '&': buf.append("&amp;"); break;
 				default: buf.append(c);
 			}
 		}
