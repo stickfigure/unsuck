68e2efa83712588822b4cc589e792df1a5500ccc
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2011-10-11 00:47:22 +0000

    Added some more reflection masking tools to unsuck

--- src/unsuck/lang/ReflectionUtils.java
+++ src/unsuck/lang/ReflectionUtils.java
@@ -1,5 +1,7 @@
 package unsuck.lang;
 
+import java.lang.reflect.Field;
+
 
 
 /**
@@ -7,9 +9,7 @@ package unsuck.lang;
  */
 public class ReflectionUtils
 {
-	/**
-	 * Converts the nasty checked exceptions to runtime exceptions
-	 */
+	/** Checked exceptions are LAME. */
 	@SuppressWarnings("unchecked")
 	public static <T> Class<T> classForName(String name)
 	{
@@ -21,5 +21,37 @@ public class ReflectionUtils
 		{
 			throw new RuntimeException(ex);
 		}
+	}
+
+	/** Checked exceptions are LAME. */
+	public static Object fieldGet(Field field, Object obj)
+	{
+		try
+		{
+			return field.get(obj);
+		}
+		catch (IllegalArgumentException e) { throw new RuntimeException(e); }
+		catch (IllegalAccessException e) { throw new RuntimeException(e); }
+	}
+
+	/** Checked exceptions are LAME. */
+	public static void fieldSet(Field field, Object obj, Object value)
+	{
+		try
+		{
+			field.set(obj, value);
+		}
+		catch (IllegalArgumentException e) { throw new RuntimeException(e); }
+		catch (IllegalAccessException e) { throw new RuntimeException(e); }
+	}
+
+	/** Checked exceptions are LAME. */
+	public static Field getField(Class<?> clazz, String fieldName) {
+		try
+		{
+			return clazz.getField(fieldName);
+		}
+		catch (SecurityException ex) { throw new RuntimeException(ex); }
+		catch (NoSuchFieldException ex) { throw new RuntimeException(ex); }
 	}
 }
