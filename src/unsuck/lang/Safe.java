686561566948c50b901d05607353cfbd7161ee7a
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2012-01-19 19:34:58 +0000

    Added the Safe class which addresses the equals() and contains() typesafety issue

---
+++ src/unsuck/lang/Safe.java
@@ -0,0 +1,27 @@
+package unsuck.lang;
+
+import java.util.Collection;
+
+
+
+/**
+ * Utility methods for comparison and list testing that are typesafe.  Gets around the annoying problem with
+ * Object.equals(), Set.contains(), etc which doesn't give you compiler checking if you change the type of the
+ * value passed in.
+ */
+public class Safe
+{
+	/**
+	 * Null safe equality comparison with type safety
+	 */
+	public static <T> boolean equals(T o1, T o2) {
+		return (o1 == o2) || ((o1 != null) && o1.equals(o2));
+	}
+
+	/**
+	 * Collection.contains() but with type safety. 
+	 */
+	public static <T> boolean contains(Collection<T> coll, T element) {
+		return coll.contains(element);
+	}
+}
