133ad448008335ce57a262ed0a7478091fae8887
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2012-02-21 16:34:34 +0000

    Added more utf8 media types

--- src/unsuck/jaxrs/MediaTypeUTF8.java
+++ src/unsuck/jaxrs/MediaTypeUTF8.java
@@ -8,5 +8,7 @@ import javax.ws.rs.core.MediaType;
  */
 public class MediaTypeUTF8 {
 	public static final String TEXT_HTML = MediaType.TEXT_HTML + "; charset=utf-8";
+	public static final String TEXT_XML = MediaType.TEXT_XML + "; charset=utf-8";
+	public static final String TEXT_PLAIN = MediaType.TEXT_PLAIN + "; charset=utf-8";
 	public static final String APPLICATION_JSON = MediaType.APPLICATION_JSON + "; charset=utf-8";
 }
