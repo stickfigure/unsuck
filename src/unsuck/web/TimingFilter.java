c3f9d7e6668bb61a176575ecc178ec52e6a25306
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2012-02-25 19:47:24 +0000

    Added TimingFilter

---
+++ src/unsuck/web/TimingFilter.java
@@ -0,0 +1,47 @@
+/*
+ */
+
+package unsuck.web;
+
+import java.io.IOException;
+import java.text.NumberFormat;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Just renders the amount of time it took to execute, in seconds
+ */
+public class TimingFilter extends AbstractFilter
+{
+	/** */
+	private final static Logger log = LoggerFactory.getLogger(TimingFilter.class);
+	
+	/** */
+	private final static NumberFormat FORMAT = NumberFormat.getNumberInstance();
+	static {
+		FORMAT.setMaximumFractionDigits(3);
+	}
+	
+	/**
+	 */
+	@Override
+	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException
+	{
+		long time = System.currentTimeMillis();
+		
+		chain.doFilter(request, response);
+		
+		if (log.isDebugEnabled()) {
+			long elapsed = System.currentTimeMillis() - time;
+			float seconds = (float)elapsed / 1000f;
+			log.debug(FORMAT.format(seconds) + " seconds");
+		}
+	}
+}
