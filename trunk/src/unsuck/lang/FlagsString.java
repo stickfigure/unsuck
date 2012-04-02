0dcece3951d3f7be18430c78af784cff4a11e1ae
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2012-04-02 06:32:45 +0000

    Added FlagsString

---
+++ trunk/src/unsuck/lang/FlagsString.java
@@ -0,0 +1,31 @@
+package unsuck.lang;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.google.common.collect.Lists;
+
+
+/**
+ * A simple tool that lets you add a bunch of strings and then convert them to a separated list.
+ * If no flags, result is empty string.
+ */
+public class FlagsString
+{
+	List<String> flags;
+	
+	public void add(String flag) {
+		if (flags == null)
+			flags = Lists.newArrayList();
+		
+		flags.add(flag);
+	}
+	
+	public String toString() {
+		if (flags == null)
+			return "";
+		else
+			return StringUtils.join(flags, "; ");
+	}
+}
