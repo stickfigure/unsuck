ff9bc88cae828e2fd1c0ca161347a7b565250c8e
lhoriman <lhoriman@25d31e78-ca52-11de-ba9f-01e9bd544a36>
2012-04-14 21:59:06 +0000

    Updated to Jackson 2.0 Upgraded guava

--- trunk/src/unsuck/json/BetterObjectMapper.java
+++ trunk/src/unsuck/json/BetterObjectMapper.java
@@ -3,8 +3,8 @@
 
 package unsuck.json;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 /**
